f477c411947f5d792745674d478e62c18428fdec
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2026-03-14 01:28:29 +0800

    Change the path to the linked CSS file and successfully linked the CSS file.

--- Browse_Restaurants/categories.php
+++ Browse_Restaurants/categories.php
@@ -22,7 +22,7 @@ if (!$categoriesResult) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Browse Restaurants</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="/CP3407-main/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
--- Browse_Restaurants/search_restaurants.php
+++ Browse_Restaurants/search_restaurants.php
@@ -47,7 +47,7 @@ if (isset($_GET['category']) && trim($_GET['category']) !== "") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search Restaurants</title>
-    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="/CP3407-main/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 <body>
